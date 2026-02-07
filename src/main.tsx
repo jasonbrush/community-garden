d7650dd47daeb5781618e82867b9a0936d73c2ea
Brush <jbrush@gmail.com>
2026-02-07 13:05:20 -0800

    Test: trivial PublicPage + main log

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-// Cloudscape global styles
 import '@cloudscape-design/global-styles/index.css'
-
-// Your app styles (optional, from the Vite template)
 import './index.css'
-
-// Public page component
 import { PublicPage } from './pages/PublicPage'
 
 console.log('*** MAIN.TSX RENDERING PUBLICPAGE ***')
